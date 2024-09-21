13ebad9887510000fe10a084cc06244f99e0a209
MrDaplyere <129137538+MrDaplyere@users.noreply.github.com>
2024-09-20 20:24:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ import datetime
 import pytz
 
 con = mysql.connector.connect(
-  host="185.232.14.52",
-  database="u760464709_tst_sep",
-  user="u760464709_tst_sep_usr",
-  password="dJ0CIAFF="
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
 )
 
 app = Flask(__name__)
@@ -22,13 +22,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     con.close()
-  
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
     con.close()
-  
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
@@ -38,17 +36,17 @@ def alumnosGuardar():
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
 
-    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+    return f"telefono: {telefono} Nombre y Apellido: {nombreapellido}"
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
     args = request.args
     pusher_client = pusher.Pusher(
-      app_id="1714541",
-      key="cda1cc599395d699a2af",
-      secret="9e9c00fc36600060d9e2",
-      cluster="us2",
-      ssl=True
+        app_id='1767967',
+        key='34091ea15b1a362fb38d',
+        secret='9a986831a832e499c9e4',
+        cluster='us2',
+        ssl=True
     )
 
     if not con.is_connected():
@@ -56,7 +54,7 @@ def registrar():
     cursor = con.cursor()
     
     sql = "INSERT INTO tst0_reservas (Nombre_Apellido, Telefono, Fecha) VALUES (%s, %s, %s)"
-    val = (args["na"], args["te"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    val = (args["nombreapellido"], args["telefono"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
     cursor.execute(sql, val)
 
     con.commit()

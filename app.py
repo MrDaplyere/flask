683fb9ea6c8dc221afa3c2fb5564feb61d3e7245
MrDaplyere <129137538+MrDaplyere@users.noreply.github.com>
2024-10-07 18:31:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify, make_response
+
 import pusher
 import mysql.connector
 import datetime
 import pytz
 
-# Conexión a la base de datos
 con = mysql.connector.connect(
     host="185.232.14.52",
     database="u760464709_tst_sep",
@@ -27,112 +30,83 @@ def alumnos():
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
     con.close()
-    matricula = request.form["txtMatriculaFA"]
+    matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
-    telefono = request.form["txtTelefonoFA"]  # Asegúrate de tener este campo en el formulario
+    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
-    return f"Teléfono: {telefono}, Nombre y Apellido: {nombreapellido}"
-
-@app.route("/registrar", methods=["GET"])
-def registrar():
-    args = request.args
-    pusher_client = pusher.Pusher(
-        app_id='1767930',
-        key='e6d3475eaa59a14fec17',
-        secret='c9dd4d864a7413ae936d',
-        cluster='us2',
-        ssl=True
-    )
-
-    if not con.is_connected():
-        con.reconnect()
-    cursor = con.cursor()
-    
-    sql = "INSERT INTO tst0_reservas (Nombre_Apellido, Telefono, Fecha) VALUES (%s, %s, %s)"
-    val = (args["na"], args["te"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
-    cursor.execute(sql, val)
-
-    con.commit()
-    con.close()
- 
-    pusher_client.trigger("conexion", "evento", args)
-    return args
-
-# Ruta para buscar los registros en la tabla 'tst0_reservas'
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
         con.reconnect()
 
     cursor = con.cursor(dictionary=True)
-    cursor.execute("SELECT Id_Reserva, Nombre_Apellido, Telefono, DATE_FORMAT(Fecha, '%d/%m/%Y') AS Fecha FROM tst0_reservas ORDER BY Id_Reserva DESC")
+    cursor.execute("""
+    SELECT Id_Log, Nombre_Apellido, Numero_Telefono, DATE_FORMAT(Fecha_Hora, '%d/%m/%Y') AS Fecha FROM contactos_log
+    ORDER BY Id_Log DESC
+    LIMIT 10 OFFSET 0
+    """)
     registros = cursor.fetchall()
-
     con.close()
 
     return make_response(jsonify(registros))
 
-# Ruta para editar los registros
 @app.route("/editar", methods=["GET"])
 def editar():
     if not con.is_connected():
         con.reconnect()
 
     id = request.args["id"]
-
     cursor = con.cursor(dictionary=True)
-    sql = "SELECT Id_Reserva, Nombre_Apellido, Telefono FROM tst0_reservas WHERE Id_Reserva = %s"
-    val = (id,)
-
+    sql    = """
+    SELECT Id_Log, Nombre_Apellido, Numero_Telefono FROM contactos_log
+    WHERE Id_Log = %s
+    """
+    val    = (id,)
     cursor.execute(sql, val)
     registros = cursor.fetchall()
     con.close()
 
     return make_response(jsonify(registros))
 
-# Ruta para guardar registros
 @app.route("/guardar", methods=["POST"])
 def guardar():
     if not con.is_connected():
         con.reconnect()
 
-    id = request.form["id"]
+    id             = request.form["id"]
     nombreapellido = request.form["nombreapellido"]
-    telefono = request.form["telefono"]
-    fechahora = datetime.datetime.now(pytz.timezone("America/Matamoros"))
-
+    numerotelefono = request.form["numerotelefono"]
+    fechahora      = datetime.datetime.now(pytz.timezone("America/Matamoros"))
+    
     cursor = con.cursor()
 
     if id:
         sql = """
-        UPDATE tst0_reservas SET
+        UPDATE contactos_log SET
         Nombre_Apellido = %s,
-        Telefono = %s
-        WHERE Id_Reserva = %s
+        Numero_Telefono = %s
+        WHERE Id_Log = %s
         """
-        val = (nombreapellido, telefono, id)
+        val = (nombreapellido, numerotelefono, id)
     else:
         sql = """
-        INSERT INTO tst0_reservas (Nombre_Apellido, Telefono, Fecha)
-        VALUES (%s, %s, %s)
+        INSERT INTO contactos_log (Nombre_Apellido, Numero_Telefono, Fecha_Hora)
+                        VALUES (%s,              %s,              %s)
         """
-        val = (nombreapellido, telefono, fechahora)
+        val =                 (nombreapellido, numerotelefono, fechahora)
     
     cursor.execute(sql, val)
     con.commit()
     con.close()
 
     pusher_client = pusher.Pusher(
-        app_id="1767930",
+        app_id="1767930",  # Cambia esto a tu nueva app_id de Pusher
         key="e6d3475eaa59a14fec17",
         secret="c9dd4d864a7413ae936d",
         cluster="us2",
         ssl=True
     )
 
-    pusher_client.trigger("conexion", "evento", {})
+    pusher_client.trigger("canalRegistrosContactos", "registroContactos", {})
 
     return jsonify({})
-
-if __name__ == "__main__":
-    app.run(debug=True)
